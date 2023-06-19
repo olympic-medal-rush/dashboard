87c544a4bdc8651f72af6a6a6ac438842ded5549
Titouan Pellerin <titouan.pellerin@gmail.com>
2023-06-19 22:29:33 +0200

    added data autofill

--- src/components/Forms/AddMedal.vue
+++ src/components/Forms/AddMedal.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import MedalForm from './MedalForm.vue'
-
+import placeholderData from '../../assets/json/medals_data.json'
 const nbMedals = ref(1)
 const medalsForm = ref([])
 
@@ -11,11 +11,16 @@ const submit = () => {
   
   medalsForm.value.forEach(medalForm => {
     data.push({
-      type: parseInt(medalForm.medalType.value),
-      titre: medalForm.medalTitle.value,
-      iso: medalForm.medalPays.value,
-      zone: parseInt(medalForm.medalZone.value),
-      athlete: medalForm.medalAthlete.value
+      type: parseInt(medalForm.type.value),
+      athlete: medalForm.athlete.value,
+      nationality: medalForm.nationality.value,
+      event_name: medalForm.eventName.value,
+      sport: medalForm.sport.value,
+      datetime: medalForm.datetime.value,
+      city: medalForm.city.value,
+      description: medalForm.description.value,
+      cover_url: medalForm.coverUrl.value,
+      zone: parseInt(medalForm.zone.value),
     })
   })
   
@@ -36,10 +41,10 @@ const handleNbMedalChange = (e) => {
 
 <template>
   <div class="Form AddMedal">
-    <h3>Ajouté des médailles</h3>
-    <label>Nombre de médailles a ajouté : </label>
-    <input type="number" min="1" max="10" :value="nbMedals" v-on:change="handleNbMedalChange">
-    <MedalForm v-for="num in nbMedals" :key="num" :idMedal="num" ref="medalsForm"/>
+    <h3>Ajouter des médailles</h3>
+    <label>Nombre de médailles à ajouter : </label>
+    <input type="number" min="1" max="3" :value="nbMedals" v-on:change="handleNbMedalChange">
+    <MedalForm v-for="num in nbMedals" :key="num" :idMedal="num" ref="medalsForm" :medal="placeholderData[Math.floor(Math.random() * placeholderData.length)] "/>
     <br>
     <a class="submit" v-on:click="submit" >ENVOYER</a>
   </div>
--- src/components/Forms/MedalForm.vue
+++ src/components/Forms/MedalForm.vue
@@ -9,54 +9,82 @@ const props = defineProps({
   idMedal: {
     type: Number,
     required: true
+  },
+  medal: {
+    type: Object,
+    required: false
   }
 })
 
 // Ref
-const medalTitle = ref()
-const medalType = ref()
-const medalZone = ref()
-const medalAthlete = ref()
-const medalPays = ref()
+const eventName = ref()
+const sport = ref()
+const datetime = ref()
+const city = ref()
+const type = ref()
+const zone = ref()
+const athlete = ref()
+const nationality = ref()
+const description = ref()
+const coverUrl = ref()
 
 defineExpose({
-  medalTitle,
-  medalType,
-  medalZone,
-  medalAthlete,
-  medalPays
+  eventName,
+  sport,
+  datetime,
+  city,
+  type,
+  zone,
+  athlete,
+  nationality,
+  description,
+  coverUrl,
 })
 </script>
 
 <template>
   <h5>Médaille n°{{props.idMedal}}</h5>
   <form action="" method="post">
-      <label for="medalTitle">Nom de l'épreuve : </label>
-      <input type="text" id="medalTitle" ref="medalTitle">
+      <label for="event_name">Nom de l'événement : </label>
+      <input type="text" id="event_name" ref="eventName" :value="props.medal?.event_name">
       <br>
-      <label for="type">Choisir un type : </label>
-      <select ref="medalType" id="type">
+      <label for="sport">Sport :</label>
+      <input type="text" id="sport" ref="sport" :value="props.medal?.sport">
+      <br>
+      <label for="datetime">Date et heure :</label>
+      <input type="datetime" id="datetime" ref="datetime" :value="props.medal?.datetime">
+      <br>
+      <label for="city">Ville :</label>
+      <input type="city" id="city" ref="city" :value="props.medal?.city">
+      <br>
+      <label for="type">Type de médaille : </label>
+      <select ref="type" id="type" :value="props.medal?.type">
         <option disabled value="">Choisir un type</option>
         <option value="2">Or</option>
         <option value="1">Argent</option>
         <option value="0">Bronze</option>
       </select>
       <br>
-      <label for="zone">Choisir une zone : </label>
-      <select ref="medalZone" id="zone">
+      <label for="zone">Zone d'apparation : </label>
+      <select ref="zone" id="zone" :value="2">
         <option disabled value="">Choisir une zone</option>
         <option v-for="data in Object.keys(terrainData.mapping)" :value="data" :key="data">{{ terrainData.mapping[data] }}</option>
       </select>
       <br>
       <label for="medalAthlete">Athlète de l'épreuve : </label>
-      <input type="text" id="medalAthlete" ref="medalAthlete">
+      <input type="text" id="medalAthlete" ref="athlete" :value="props.medal?.athlete">
       <br>
       <label for="pays">Pays de l'athlète : </label>
-      <select ref="medalPays" id="pays">
+      <select ref="nationality" id="pays" :value="props.medal?.nationality">
         <option disabled value="">Choisir un pays</option>
         <option v-for="data in Object.keys(countriesFR.countries)" :value="countries.alpha2ToAlpha3(data)" :key="data">{{getFlagEmoji(data) }} - {{ countriesFR.countries[data] }}</option>
       </select>
       <br>
+      <label for="cover_url">Cover URL : </label>
+      <input type="text" id="cover_url" ref="coverUrl" :value="props.medal?.cover_url">
+      <br>
+      <label for="description">Description : </label>
+      <textarea rows="5" cols="50" id="description" ref="description" :value="props.medal?.description"></textarea>
     </form>
 </template>
 
